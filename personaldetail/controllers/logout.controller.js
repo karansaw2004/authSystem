4d11500788fc348ae6c844208da96fad8364f87a
sawkaranvinod <saw_ug_24@ece.nits.ac.in>
2025-08-27 19:44:57 +0530

    done

--- personaldetail/controllers/logout.controller.js
+++ personaldetail/controllers/logout.controller.js
@@ -9,10 +9,10 @@ export async function handleLogOut(req,reply) {
         const loginDetail = await Login.findOneAndUpdate({userId,deviceFingerPrintHash},{$set:{currentStatus:"deActive"}},{new:true});
         if (!loginDetail) {
             return reply.send(new ApiError("Logout failed",400));
-        }
+        };
         return reply.send(new ApiResponse({logout:true},"sucess",200));
     } catch (error) {
         console.log("error in the main handle function of the logout",error.message);
         return reply.send(new ApiError("Internal server error",500));
-    }
+    };
 };
